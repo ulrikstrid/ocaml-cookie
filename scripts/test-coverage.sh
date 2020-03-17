44d1689f6876fafcffd3b93b2322d6ad2a5e6d6c
Ulrik <ulrik.strid@outlook.com>
2020-03-17 21:51:19 +0100

    Prepare for release

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -8,12 +8,7 @@ gsed -i '$i \(preprocess (pps bisect_ppx))\' lib/dune
 echo running tests
 BISECT_ENABLE=yes REPORT_PATH=./junit.xml esy test
 
-esy echo "#{self.target_dir / 'default' / 'lib_test'}"
-
 cp $(esy echo "#{self.target_dir / 'default' / 'lib_test' / 'junit.xml'}") ./junit.xml
-
-echo copying bisect files
-cp $(esy echo "#{self.target_dir / 'default' / 'lib_test'}")/[bisect]* ./
 
 echo generating reports
 esy bisect-ppx-report html
@@ -21,5 +16,3 @@ esy bisect-ppx-report summary
 
 echo reseting files
 git checkout lib/dune
-rm *.coverage
-rm test*.{exe,ml}
